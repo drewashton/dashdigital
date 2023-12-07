18f11113b1fcc939249cb67e46ea3c2b81543ff8
Drew Ashton <147790310+drewashton@users.noreply.github.com>
2023-12-07 01:10:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-// Function to check if an element is in the viewport
-function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
+const header = document.querySelector('header');
+const headerHeight = header.offsetHeight;
+
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > headerHeight) {
+    header.classList.add('sticky-header');
+    setTimeout(() => {
+      header.classList.remove('sticky-header');
+    }, 1000); // Adjust the duration the header stays sticky (in milliseconds)
+  }
+});
 
 // Function to handle the scrolling animation
 function handleScrollAnimation(entries) {
@@ -27,22 +30,6 @@ document.querySelectorAll('.des-images img').forEach((image) => {
   scrollObserver.observe(image);
 });
 
-
-
-
-// ----------------------------------------------------------
-
-// Function to check if an element is in the viewport
-function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
 // Function to handle the intersection observer callback for rotate fade-in animation
 function handleIntersection(entries, observer) {
   entries.forEach(entry => {
@@ -60,9 +47,62 @@ function handleIntersection(entries, observer) {
   });
 }
 
-// Set up the Intersection Observer for elements with the class 'rotate-on-scroll'
+// Set up the Intersection Observer for elements with the class 'services-image'
 const rotateObserver = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
 
 document.querySelectorAll('.services-image img').forEach(element => {
   rotateObserver.observe(element);
 });
+
+// Function to handle the intersection observer callback for typewriter animation
+function handleTypewriterIntersection(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // If the element is in the viewport, trigger the typewriter effect
+      typeWriterEffect(entry.target);
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+// Set up the Intersection Observer for elements with the class 'typewriter-heading'
+const typewriterObserver = new IntersectionObserver(handleTypewriterIntersection, { threshold: 0.5 });
+
+// Apply the observer to all section headings
+document.querySelectorAll('h2:not(.no-typewriter-effect)').forEach(heading => {
+  typewriterObserver.observe(heading);
+});
+
+// Function to simulate a typewriter effect with spaces and a cursor
+function typeWriterEffect(element) {
+  const words = element.innerText.split(' ');
+  element.innerHTML = ''; // Use innerHTML to preserve non-breaking spaces
+  let currentWordIndex = 0;
+  let currentCharIndex = 0;
+
+  function type() {
+    if (currentWordIndex < words.length) {
+      const word = words[currentWordIndex];
+      if (currentCharIndex <= word.length) {
+        const char = word.charAt(currentCharIndex);
+        element.innerHTML += char === ' ' ? '&nbsp;' : char; // Use non-breaking space
+        currentCharIndex++;
+        setTimeout(type, 50); // Adjust the typing speed (in milliseconds)
+      } else {
+        // Move to the next word and add a space
+        currentWordIndex++;
+        currentCharIndex = 0;
+        element.innerHTML += '&nbsp;'; // Add a non-breaking space
+        setTimeout(type, 50); // Adjust the typing speed (in milliseconds)
+      }
+    } else {
+      // Add a blinking cursor when typing is complete
+      element.innerHTML += '<span class="cursor">|</span>';
+      const cursor = document.querySelector('.cursor');
+      cursor.style.animation = 'blink-cursor 0.7s infinite'; // Start blinking after typing
+    }
+  }
+
+  type();
+}
+
